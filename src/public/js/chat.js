59bb9f4c0e734157a6adb43c58aadf84663d3b26
Clayderson Ferreira <contato@clayderson.com.br>
2018-01-13 19:24:13 -0300

    O frontend agora consegue criar ou logar um pseudo usuário e obter as mensagens da sala

---
+++ src/public/js/chat.js
@@ -0,0 +1,134 @@
+$(document).ready(function() {
+	$.cookie('user') === undefined ? userRegister() : userLogin();
+
+	function fatalError(errorMessage) {
+		$.removeCookie('user');
+		window.location.reload(true);
+	}
+
+	function getMessages() {
+		$.ajax({
+			url: `${runtime.api.url}/v1/messages/${runtime.room.id}`,
+			contentType: 'application/json; charset=UTF-8',
+			type: 'GET',
+			dataType: 'json',
+			success: function(roomMessages) {
+				let youOrHe = null;
+				for(let id in roomMessages) {
+					runtime.lastId = id;
+					youOrHe = roomMessages[id]['userId'] === runtime.user.id ? 'you' : 'he';
+					messagesContainer.append(`<div class="${youOrHe}"><p title="${roomMessages[id]['createdAt']}">${roomMessages[id]['message']}</p></div>`);
+				}
+
+				messagesContainer.append(`<div class="system"><p>Este chat é um projeto experimental. Você <strong>não</strong> deve compartilhar dados sensíveis aqui. O chat é visível para qualquer pessoa que possuir o link e novos visitantes só poderão ver mensagens enviadas nos últimos 15 minutos.</p></div>`);
+				messagesContainer.append(`<div class="system"><p>Convide seus amigos para o bate papo enviando esse link para eles &#187; <a href="${runtime.room.url}">${runtime.room.url}</a></p></div>`);
+
+				messagesContainer.show();
+				messagesLoading.hide();
+				inputMessage.prop('disabled', false);
+
+				viewportContainer.animate({ scrollTop: viewportContainer.prop('scrollHeight') }, 500);
+
+				setInterval(function() {
+					getNewMessages();
+				}, 1300);
+			},
+			error: function(jqXHR) {
+				setTimeout(function() {
+					getMessages(runtime.room.id);
+				}, 2000);
+			}
+		});
+	}
+
+	function getNewMessages() {
+		if (runtime.getingNewMessage === false) {
+			$.ajax({
+				url: `${runtime.api.url}/v1/messages/${runtime.room.id}/${runtime.lastId}/20`,
+				contentType: 'application/json; charset=UTF-8',
+				type: 'GET',
+				dataType: 'json',
+				beforeSend: function() {
+					runtime.getingNewMessage = true;
+				},
+				complete: function() {
+					runtime.getingNewMessage = false;
+				},
+				success: function(roomMessages) {
+					let forceScroll = false;
+					if (viewportContainer.scrollTop() + viewportContainer.prop('clientHeight') === viewportContainer.prop('scrollHeight')) {
+						forceScroll = true;
+					}
+
+					let youOrHe = null;
+					for(let id in roomMessages) {
+						runtime.lastId = id;
+						youOrHe = roomMessages[id]['userId'] === runtime.user.id ? 'you' : 'he';
+						messagesContainer.append(`<div class="${youOrHe}"><p title="${roomMessages[id]['createdAt']}">${roomMessages[id]['message']}</p></div>`);
+					}
+
+					if (forceScroll) {
+						viewportContainer.animate({ scrollTop: viewportContainer.prop('scrollHeight') }, 500);
+					}
+
+					alertContainer.hide(300);
+				},
+				error: function(jqXHR) {
+					alertContainer.show(300);
+				}
+			});
+		}
+	}
+
+	function userRegister() {
+		$.ajax({
+			url: `${runtime.api.url}/v1/users`,
+			contentType: 'application/json; charset=UTF-8',
+			type: 'POST',
+			dataType: 'json',
+			success: function(response) {
+				if (setUserCredentials(response)) {
+					getMessages();
+				}
+			},
+			error: function(jqXHR) {
+				fatalError('Falha ao tentar registrar usuário. A página será atualizada para uma nova tentativa.');
+			}
+		});
+	}
+
+	function userLogin() {
+		if (JSON.parse($.cookie('user')).token === undefined) {
+			fatalError('Falha ao tentar obter token do usuário. A página será atualizada.');
+			return;
+		}
+
+		$.ajax({
+			url: `${runtime.api.url}/v1/users/${JSON.parse($.cookie('user')).token}`,
+			contentType: 'application/json; charset=UTF-8',
+			type: 'GET',
+			dataType: 'json',
+			success: function(response) {
+				if (setUserCredentials(response)) {
+					getMessages();
+				}
+			},
+			error: function(jqXHR) {
+				fatalError('Falha ao tentar obter informações do usuário. A página será atualizada para uma nova tentativa.');
+			}
+		});
+	}
+
+	function setUserCredentials(data) {
+		if (data.id !== undefined && data.token !== undefined) {
+			$.cookie('user', JSON.stringify({ token: data.token }), { path: '/' });
+
+			runtime.user.id = data.id;
+			runtime.user.token = data.token;
+
+			return true;
+		}
+
+		fatalError('Falha ao tentar definir cookie de credenciais do usuário. A página será atualizada.');
+	}
+});
