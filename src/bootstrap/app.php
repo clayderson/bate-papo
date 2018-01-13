dc7d87df1b61b712e1c291d3db1599cab2c25b77
Clayderson Ferreira <contato@clayderson.com.br>
2018-01-12 23:45:44 -0300

    Middlewares ajustados para se adequarem melhor ao tipo de requisição: interface do usuário / api

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -7,6 +7,10 @@
 	ini_set('display_errors', 1);
 	ini_set('display_startup_errors', 1);
 
+	if (__LOGGER) {
+		logger::setFile(__LOGGER_FILE);
+	}
+
 	$app = new \Slim\App([
 		'settings' => [
 			'displayErrorDetails' => __APP_DEBUG,
@@ -16,83 +20,129 @@
 
 	$container = $app->getContainer();
 
-	if (__LOGGER) {
-		logger::setFile(__LOGGER_FILE);
+	if (stripos($container['request']->getUri()->getPath(), '/api') !== false) {
+		if (__MYSQL_HOST) {
+			db::credentials(
+				__MYSQL_HOST,
+				__MYSQL_USER,
+				__MYSQL_PASS,
+				__MYSQL_DBNAME,
+				__MYSQL_PORT
+			);
+		}
+
+		$container['notFoundHandler'] = function($container) {
+			return function($request, $response) use ($container) {
+				return $response->withJson([
+					'errorMessage' => 'not found'
+				], 404);
+			};
+		};
+
+		$container['notAllowedHandler'] = function($container) {
+			return function($request, $response, $methods) use ($container) {
+				return $response->withJson([
+					'errorMessage' => 'method not allowed'
+				], 405);
+			};
+		};
+
+		$container['errorHandler'] = function($container) {
+			return function($request, $response, $e) use ($container) {
+				if (is_object($e)) {
+					$errorMessage = "{$e->getMessage()} {$e->getFile()} {$e->getLine()}";
+					logger::add(logger::ERROR, $errorMessage);
+				}
+
+				if (!isset($errorMessage) || !$container->get('settings')['displayErrorDetails']) {
+					$errorMessage = 'Algo não vai bem com nossa API';
+				}
+
+				return $response->withJson([
+					'errorMessage' => $errorMessage
+				], 500);
+			};
+		};
+
+		$container['phpErrorHandler'] = function($container) {
+			return $container['errorHandler'];
+		};
+	} else {
+		$container['notFoundHandler'] = function($container) {
+			return function($request, $response) use ($container) {
+				$specialPaths = ['/favicon.ico'];
+				if (in_array($request->getUri()->getPath(), $specialPaths)) {
+					return $response->withStatus(404);
+				} else {
+					logger::add(
+						logger::WARNING,
+						"Página não encontrada: {$request->getUri()->getPath()}"
+					);
+
+					return $container->view->render($response->withStatus(404), '/error.twig', [
+						'statusCode' => 404,
+						'errorMessage' => 'Página não encontrada',
+					]);
+				}
+			};
+		};
+
+		$container['notAllowedHandler'] = function($container) {
+			return function($request, $response, $methods) use ($container) {
+				return $container->view->render($response->withStatus(405), '/error.twig', [
+					'statusCode' => 405,
+					'errorMessage' => 'Método não permitido',
+				]);
+			};
+		};
+
+		$container['errorHandler'] = function($container) {
+			return function($request, $response, $e) use ($container) {
+				if (is_object($e)) {
+					$errorMessage = "{$e->getMessage()} {$e->getFile()} {$e->getLine()}";
+					logger::add(logger::ERROR, $errorMessage);
+				}
+
+				if (!isset($errorMessage) || !$container->get('settings')['displayErrorDetails']) {
+					$errorMessage = 'Algo não vai bem com nossos servidores';
+				}
+
+				return $container->view->render($response->withStatus(500), '/error.twig', [
+					'statusCode' => 500,
+					'errorMessage' => $errorMessage,
+				]);
+			};
+		};
+
+		$container['phpErrorHandler'] = function($container) {
+			return $container['errorHandler'];
+		};
+
+		$container['view'] = function($container) {
+			$view = new \Slim\Views\Twig(__DIR_VIEWS, [
+				'cache' => false,
+				'charset' => 'utf-8',
+				'strict_variables' => __APP_DEBUG,
+				'debug' => __APP_DEBUG,
+			]);
+
+			$basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
+			$view->addExtension(new \Slim\Views\TwigExtension($container['router'], $basePath));
+
+			$view->getEnvironment()->addGlobal('url', [
+				'current' => $container['request']->getUri()->getPath() . '?' . $container['request']->getUri()->getQuery(),
+				'previous' => $container['request']->getHeaders()['HTTP_REFERER'][0] ?? '../',
+			]);
+
+			$view->getEnvironment()->addGlobal('git', [
+				'commit' => substr(shell_exec('git rev-parse HEAD'), 0, 7),
+			]);
+
+			return $view;
+		};
 	}
 
-	db::credentials(__MYSQL_HOST, __MYSQL_USER, __MYSQL_PASS, __MYSQL_DBNAME, __MYSQL_PORT);
-
-	$container['notFoundHandler'] = function($container) {
-		return function($request, $response) {
-			logger::add(
-				logger::WARNING,
-				"Página não encontrada: {$request->getUri()->getPath()}"
-			);
-
-			$specialPaths = ['/', '/favicon.ico'];
-
-			if (in_array($request->getUri()->getPath(), $specialPaths)) {
-				return $response->withJson([
-					'errorMessage' => 'Not found'
-				], 404);
-			}
-
-			return $response->withStatus(301)->withHeader('Location', '/?redirectReason=404');
-		};
-	};
-
-	$container['notAllowedHandler'] = function($container) {
-		return function($request, $response, $methods) {
-			return $response->withStatus(301)->withHeader('Location', '/?redirectReason=405');
-		};
-	};
-
-	$container['errorHandler'] = function($container) {
-		return function($request, $response, $e) use ($container) {
-			if (is_object($e)) {
-				$errorMessage = "{$e->getMessage()} {$e->getFile()} {$e->getLine()}";
-				logger::add(logger::ERROR, $errorMessage);
-
-				if (!$container->get('settings')['displayErrorDetails']) {
-					unset($errorMessage);
-				}
-			}
-
-			return $container->view->render($response->withStatus(500), '/error.twig', [
-				'errorMessage' => $errorMessage ?? 'Modo de debug desabilitado',
-			]);
-		};
-	};
-
-	$container['phpErrorHandler'] = function($container) {
-		return $container['errorHandler'];
-	};
-
-	$container['view'] = function($container) {
-		$view = new \Slim\Views\Twig(__DIR_VIEWS, [
-			'cache' => false,
-			'charset' => 'utf-8',
-			'strict_variables' => __APP_DEBUG,
-			'debug' => __APP_DEBUG,
-		]);
-
-		$basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
-		$view->addExtension(new \Slim\Views\TwigExtension($container['router'], $basePath));
-
-		$view->getEnvironment()->addGlobal('url', [
-			'resolver' => $container['request']->getUri()->getHost(),
-			'current' => $container['request']->getUri()->getPath() . '?' . $container['request']->getUri()->getQuery(),
-			'previous' => $container['request']->getHeaders()['HTTP_REFERER'][0] ?? '../',
-		]);
-
-		$view->getEnvironment()->addGlobal('git', [
-			'commit' => substr(shell_exec('git rev-parse HEAD'), 0, 7),
-		]);
-
-		return $view;
-	};
-
-	require __DIR_BOOTSTRAP . '/routes/website.php';
 	require __DIR_BOOTSTRAP . '/routes/api.php';
+	require __DIR_BOOTSTRAP . '/routes/website.php';
 
 	$app->run();
